6f759f6c3392205c134882c445d2ea41531eb89b
Jayesh Ranjan <jranjan@cs.stonybrook.edu>
2016-12-12 03:29:25 -0500

    ag works

--- projectClient.py
+++ projectClient.py
@@ -1,19 +1,21 @@
 import sys
 import os
 import fnmatch
-from socket import *
+import socket
 currentUserID = "" #This will hold the user id info
+clientSocket = None
 #DISCUSSIONS IS INPUT FROM SERVER THIS IS JUST TO TEST GROUPS
-discussions = ["Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"]
-
+fromServerAG= "Group 1`Group 2`Group 3`Group 4`Group 5`Group 6"
 def main():
 	argv = input("Type in your login ID: ")
+	global clientSocket
+	clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	clientSocket.connect(("allv24.all.cs.stonybrook.edu", 6789))
 	loginHandler(argv)
 	while(1):
 		argv = input("Enter command: ")
 		argv = argv.split(" ")
 		parseArgs(argv)
-
 def parseArgs(argv):
 	if argv[0] == "ag":
 		agHandler(argv)
@@ -23,6 +25,7 @@ def parseArgs(argv):
 		rgHandler(argv)
 	elif argv[0] == "logout":
 		print(str(currentUserID) + " is logging out")
+		# clientSocket.close()
 		sys.exit(0)
 	else:
 		print("Invalid command")
@@ -40,15 +43,35 @@ def loginHandler(argv):
 
 def rgHandler(argv):#INCOMPLETE
 	print(argv)
-	if(len(argv)==1):
+	groupName = ""
+	n = 0
+	if(len(argv)==2):#Contains rg and the group name
 		n = 2
-	elif (len(argv)==2):
-		n = argv[1]
+		groupName = argv[1]
+	elif (len(argv)==2):#Contains rg , group name and n
+		groupName = argv[1]
+		n = int(argv[2])
 	else:
 		print("Illegal arguements detected")
 		return
-	
-
+	# postMessage = ["Message1","Message2","Message3","Message4","Message5","Message6"]
+	# dateMessage = ["Nov 2016","Dec 2016","Jan 2017","Feb 2017","Mar 2017","Apr 2017"]
+	# unread = 5
+	# listRead = []
+	# for i in range(len(postMessage)):
+	# 	if(i < unread):
+	# 		if(str(i+1) not in listRead)
+	# 			print(str(i+1)+". N " + dateMessage[i] +" "+postMessage[i])
+	# 		else:
+	# 			print(str(i+1)+".   " + dateMessage[i] +" "+postMessage[i])				
+	# 	else:
+	# 		print(str(i+1)+".   " + dateMessage[i] +" "+postMessage[i])
+ #    	if ((i+1) % n == 0 and i != 0) or i == len(postMessage) - 1:
+	# 		userInput = input("Type:\nn lists the next "+str(n) +" posts\nListChoose: ").split(" ")
+	#         if(userInput[0]=="n"):
+	#             continue
+	#         elif(userInput[0]=="r"):
+	#         	listRead = userInput[0].split("-")
 
 def agHandler(argv):
 	if(len(argv)==1):
@@ -62,7 +85,21 @@ def agHandler(argv):
 	userData = open(currentUserID,"r+")
 	userDataRead = userData.read()
 	subscribedGroupList = userDataRead.split("\n")#Lists all the groups the current user is suscribed to
-	print(subscribedGroupList)
+	# subscribedGroupList = []
+	argument = str.encode(argv[0])
+	clientSocket.sendall(argument)
+	serverGroupsName = clientSocket.recv(4096)#fromServerAG.split("`")
+	decodedString = serverGroupsName.decode("utf-8")
+	discussions = decodedString.split(",")
+	print(discussions)
+	# for i in range(len(rawSubscribedGroupList)):
+	# 	groupName = rawSubscribedGroupList[i].split(" ")
+	# 	string = ""
+	# 	for j in range(len(groupName)-1):
+	# 		string = groupName[i] + " "
+	# 	subscribedGroupList.append(string)
+	# print(subscribedGroupList)
+
 	for i in range(1, len(discussions)+1):
 		if (discussions[i-1] not in subscribedGroupList):
 			print(str(i) + "\t( )\t" + discussions[i-1])
@@ -79,7 +116,7 @@ def agHandler(argv):
 			elif userInput[0] == "s":
 				if (len(userInput) > 1):
 					for j in range(1, len(userInput)):
-						if int(userInput[j])-1 > len(discussions)-1 or int(userInput[j])-1 < 1:
+						if (int(userInput[j])-1 > len(discussions)-1) or int(userInput[j]) < 1:
 							print("Invalid index")
 						else:
 							if discussions[int(userInput[j])-1] not in subscribedGroupList:
@@ -172,12 +209,11 @@ def sgHandler(argv):
 	for a in newSubscribedGroupList:
 		if len(a) > 1:
 			newList.append(a)
-			
+
 	userData = open(currentUserID,"w+")
 	for i in range(len(newList)):
 		userData.write(newList[i]+"\n")
 
 	userData.close() #closing file
-
 
 main();
